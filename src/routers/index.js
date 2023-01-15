ff964db1dbe0d7fa5b4e2192e51e0029a1e65bf3
shamim-al-mamun <shamimalmamun.aiub@gmail.com>
2023-01-15 23:10:18 +0600

    login

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,8 +8,8 @@ const Routers = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Splash" component={Splash} />
-      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 };
